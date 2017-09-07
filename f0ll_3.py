b5c324453d1e31b603bbfcb8330d6b0e5c05145c
Ððµ¢hêßåg <yesimdingo@gmail.com>
2017-09-07 14:58:23 +0000

    1

---
+++ f0ll_3.py
@@ -0,0 +1,40 @@
+#7.9.2017
+#Ingvar Vigfússon
+#föll æfingaverkefni 3
+
+#föll
+
+def skrifaUt(listi):
+    print(listi)
+
+def staersta(listi):
+    staersta = listi[0]
+    for num in listi:
+        if staersta < num:
+            staersta = num
+    return staersta
+
+def minnsta(listi):
+    minnsta = listi[0]
+    for num in listi:
+        if minnsta > num:
+            minnsta = num
+    return minnsta
+
+def summa(listi):
+    summa = 0
+    for x in listi:
+        
+
+#forrit
+
+listi = []
+for x in range(5):
+    tala = int(input("Sláðu inn tölu "))
+    listi.append(tala)
+
+skrifaUt(listi) #birta lista
+
+print(staersta(listi)) #birtir stærstu töluna
+
+print(minnsta(listi)) #birtir minnstu töluna
