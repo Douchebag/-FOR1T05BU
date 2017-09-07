ef4cbaf684b2bf02d0a279afa35aed3f6f05a53c
Ððµ¢hêßåg <yesimdingo@gmail.com>
2017-09-07 14:30:54 +0000

    föll 2

---
+++ f0ll_2.py
@@ -0,0 +1,51 @@
+#7.9.2017
+#Ingvar Vigfússon
+#föll æfingaverkefni 2
+
+#föll
+def reiknaRummalKulu(radius):
+    rummalKulu = (4.0 * 3.14 * (radius * radius* radius)) / 3.0
+    return rummalKulu
+
+def reiknaRummalKassa(lengd, breidd, haed):
+    rummalKassa = lengd * breidd * haed
+    return rummalKassa
+
+def reiknaFlatarmalThrihyrnings(breidd, haed):
+    flatarmalThri = (breidd * haed) / 2
+    return flatarmalThri
+
+#valmynd
+valm=""
+while valm !="6":
+    print("\n--------------")
+    print("1. Rúmmál kúlu")
+    print("2. Rúmmál kassa")
+    print("3. Flatarmál þríhyrnings")
+    print("4. Hætta")
+    print("--------------")
+    valm=input("")
+
+    if valm == "1":
+        radius = int(input("Hver er radius hringsins? "))
+        print(reiknaRummalKulu(radius))
+
+    elif valm == "2":
+        lengd = int(input("Hver er lengd þríhyrningsins? "))
+        breidd = int(input("Hver er breidd þríhyrningsins? "))
+        haed = int(input("Hver er haed þríhyrningsins? "))
+
+        print(reiknaRummalKassa(lengd, breidd, haed))
+
+    elif valm == "3":
+        breidd = int(input("Hver er breidd þríhyrningsins "))
+        haed = int(input("Hver er haed þríhyrningsins "))
+
+        print(reiknaFlatarmalThrihyrnings(breidd, haed))
+
+
+
+
+
+
+
--- f0ll_functions.py
+++ f0ll_functions.py
@@ -1,12 +1,13 @@
 #Ingvar Vigfússon
 #5.9.2017
 #föll functions
+from random import *
 
-def snorri():
+def snorri():#prentar út þrjár setningar
     print("Þetta er sýnidæmi um fall(functions)")
     print("Það er ekki með neina færibreytu")
     print("Það prentar þessar línur á skjáinn")
-def konni(skilabod):
+def konni(skilabod):#birtir þrjár setningar og skilabod
     print("Fall með eina færibreytu, sem heitir skilabod")
     print("skilabod er breyta sem fær gildi þegar kallað er í fallið")
     print("Fallið framkvæmir þær aðgerðir sem eru í því þegar kallað er í það")
@@ -22,6 +23,13 @@ def minnstaTala(tala1, tala2, tala3):
         minnsta = tala3
     return minnsta
 
+def listi_summa(tolur):
+    summa=0
+    for tala in tolur:
+        summa+=tala #bætir tölunni við summa
+    return summa
+
+
 #kalla í föllin
 snorri() #kalla í fallið snorri sem hefur enga færibreytu
 print("snorri er búinn að prenta línurnar og skila stjórninni tilbaka")
@@ -36,3 +44,39 @@ setning = input("Hvað villtu segja við konna? ")
 konni(setning)
 print("-------------------")
 print("minnsta talan er:", minnstaTala(1, 2, 3))
+print("-------------------")
+tala11 = int(input("Sláðu inn tölu 1 af 3 "))
+tala22 = int(input("Sláðu inn tölu 2 af 3 "))
+tala33 = int(input("Sláðu inn tölu 3 af 3 "))
+print("minnsta talan er:", minnstaTala(tala11, tala22, tala33))
+print("-------------------")
+listi = []
+for i in range(7):
+    listi.append(randint(1, 9))
+print(listi) #prenta listann til að sjá hvaða tölur eru í honum
+print("Summa talnana í listanum er:", listi_summa(listi))
+print("-------------------")
+#set fallið hér til að ekki þurfa að scrolla upp, má en er gerir forritið ólæsilegra
+def setning(nafn='konni',action='sefur',hlutur='rólega'): # hver færibreyta með sjálfgefið gildi
+    texti = nafn + " " + action + " " + hlutur
+    return texti
+#og kalla strax hérna í fallið
+setning()
+print(setning(hlutur="lengi"))
+notandi = input("Hvað heitir þú? ")
+athofn = input("Hvað gerir þú? ")
+lysing = input("Hvernig gerir þú það? ")
+print(setning(nafn=notandi, hlutur=lysing))
+
+
+
+
+
+
+
+
+
+
+
+
+
